8f19c50f8ab557a4c2299a16d351e5f965616595
daleksprinter <b1015120@fun.ac.jp>
2019-05-09 05:34:25 +0900

    add appbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Works from './components/Works/Works'
 import Aboutme from './components/Aboutme'
 import Links from './components/Links'
 import Compprog from './components/Compprog'
 import Skills from './components/Skills'
+import { NavItem, Nav, Navbar } from 'react-bootstrap'
+
+import AppBar from '@material-ui/core/AppBar';
+
+import './App.css'
+import { Toolbar, IconButton, Button, Link, Typography} from '@material-ui/core';
+
+class Header extends React.Component{
+
+    render(){
+      
+      return(
+        <AppBar position = 'static'>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/'} color = 'inherit'>Home</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/aboutme'} color = 'inherit'>AboutMe</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/works'} color = 'inherit'>Works</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/competitive-programming'} color = 'inherit'>CompetitiveProgramming</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/skills'} color = 'inherit'>Skills</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" className = 'navitem'>
+              <Link href = {'/links'} color = 'inherit'>Links</Link>
+            </Typography>
+
+            
+            
+          </Toolbar>
+        </AppBar>
+      )
+
+    }
+}
+
 
 function App() {
   return (
     <div className = 'app'>
       <BrowserRouter>
-        <p>Rapio's Portfolio</p>
-        <Link to = '/'>Home</Link>
-        <Link to = '/works'>Works</Link>
-        <Link to = '/competitive-programming'>Competitive Programming</Link>
-        <Link to = '/aboutme'>AboutMe</Link>
-        <Link to = '/links'>Links</Link>
-        <Link to = '/skills'>Skills</Link>
+        
+        <Header />
         
         <Route exact path = '/' component = {Home}></Route>
         <Route path = '/works' component = {Works}></Route>
