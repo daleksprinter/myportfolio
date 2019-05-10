88d6b6aa26caa15750b6c8c275bd35759c508f59
daleksprinter <b1015120@fun.ac.jp>
2019-05-10 13:27:54 +0900

    update

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -25,6 +25,7 @@ const Language = () => {
                     <li>Django</li>
                     <li>Flask</li>
                     <li>React</li>
+                    <li>Express</li>
                 </ul>
 
                 <p>ミドルウェア等</p>
@@ -52,7 +53,7 @@ const Language = () => {
                     <li>Emacs</li>
                 </ul>
 
-                <p>使ってみたことがある → Processing, Ruby, PHP, Swift, Express等</p> 
+                <p>使ってみたことがある → Processing, Ruby, PHP, Swift等</p> 
                 <p>学習中 → Go</p>
 
         </Paper>
